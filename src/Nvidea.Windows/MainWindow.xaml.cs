497c8c610d73f480db4bdbed055f16ff4fed667a
UnknownGod2011 <tanushshah2006@gmail.com>
2026-09-13 09:42:11 +0530

    Restore RegisterHotKey PInvoke signature

--- src/Nvidea.Windows/MainWindow.xaml.cs
+++ src/Nvidea.Windows/MainWindow.xaml.cs
@@ -369,7 +369,7 @@ public partial class MainWindow : Window
 
     [DllImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers);
+    private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
 
     [DllImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]

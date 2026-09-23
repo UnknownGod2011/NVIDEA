a70373a88aeb1a9d038ee5cc8788b998b414d90a
UnknownGod2011 <tanushshah2006@gmail.com>
2026-09-24 04:40:46 +0530

    test: qualify canonical worker result signatures

---
+++ tests/Nvidea.Core.Tests/RemoteResearchWorkerSignatureTests.cs
@@ -0,0 +1,43 @@
+using System.Security.Cryptography;
+using Nvidea.Core.Jobs;
+
+namespace Nvidea.Core.Tests;
+
+public sealed class RemoteResearchWorkerSignatureTests
+{
+    [Fact]
+    public void Signature_VerifiesOnlyWithPinnedWorkerIdentity()
+    {
+        using var worker = RSA.Create(2048); using var other = RSA.Create(2048); var envelope = Envelope("job-a");
+        var signature = RemoteResearchWorkerSignature.Sign(envelope, worker.ExportPkcs8PrivateKeyPem());
+        RemoteResearchWorkerSignature.Verify(envelope, signature, worker.ExportSubjectPublicKeyInfoPem());
+        Assert.Throws<CryptographicException>(() => RemoteResearchWorkerSignature.Verify(envelope, signature, other.ExportSubjectPublicKeyInfoPem()));
+    }
+
+    [Fact]
+    public void Signature_RejectsForgedSignature()
+    {
+        using var worker = RSA.Create(2048); var envelope = Envelope("job-a"); var signature = Convert.FromBase64String(RemoteResearchWorkerSignature.Sign(envelope, worker.ExportPkcs8PrivateKeyPem())); signature[0] ^= 1;
+        Assert.Throws<CryptographicException>(() => RemoteResearchWorkerSignature.Verify(envelope, Convert.ToBase64String(signature), worker.ExportSubjectPublicKeyInfoPem()));
+    }
+
+    [Fact]
+    public void Signature_RejectsCrossJobReplay()
+    {
+        using var worker = RSA.Create(2048); var envelope = Envelope("job-a"); var signature = RemoteResearchWorkerSignature.Sign(envelope, worker.ExportPkcs8PrivateKeyPem());
+        Assert.Throws<CryptographicException>(() => RemoteResearchWorkerSignature.Verify(envelope with { RemoteJobId = "job-b" }, signature, worker.ExportSubjectPublicKeyInfoPem()));
+    }
+
+    [Fact]
+    public void Signature_RejectsReportReceiptCiphertextSubstitution()
+    {
+        using var worker = RSA.Create(2048); var envelope = Envelope("job-a"); var signature = RemoteResearchWorkerSignature.Sign(envelope, worker.ExportPkcs8PrivateKeyPem());
+        Assert.Throws<CryptographicException>(() => RemoteResearchWorkerSignature.Verify(envelope with { Ciphertext = Convert.ToBase64String(new byte[] { 9, 9, 9 }) }, signature, worker.ExportSubjectPublicKeyInfoPem()));
+    }
+
+    private static ProtectedResearchResultEnvelope Envelope(string remoteJobId)
+    {
+        var now = DateTimeOffset.Parse("2026-09-24T00:00:00Z");
+        return new(ResearchResultProtector.ProtocolVersion, "mY7FhPlAdtPz9xL4b8gU1cKqN3sW6vRt", remoteJobId, Convert.ToBase64String(new byte[] { 1, 2, 3 }), Convert.ToBase64String(new byte[] { 4, 5, 6 }), Convert.ToBase64String(new byte[] { 7, 8, 9 }), Convert.ToBase64String(new byte[] { 10, 11, 12 }), now, now.AddHours(1));
+    }
+}
